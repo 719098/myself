b377a191a76908573591c757281d8f958b5effee
719098 <113333214+719098@users.noreply.github.com>
2025-04-12 19:57:33 +0800

    v1

--- index.py
+++ index.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("myself.html")
+@app.route("/holland")
+def holland():
+    return render_template("holland.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
